703b213ece930a1a2374452dce537b5d458d9cd4
vietng322611 <90992374+vietng322611@users.noreply.github.com>
2021-11-08 20:44:31 +0700

    Update url

--- update.py
+++ update.py
@@ -5,9 +5,9 @@ import os
 import time
 import urllib
 
-url = "https://api.github.com/repos/vietng322611/simple-python-discord-bot/releases/latest"
+url = "https://api.github.com/repos/vietng322611/music-bot-python/releases/latest"
 response = requests.get(url)
-latest_release = response.json()["html_url"].strip("https://github.com/vietng322611/simple-python-discord-bot/releases/tag/v")
+latest_release = response.json()["html_url"].strip("https://github.com/vietng322611/music-bot-python/releases/tag/v")
 f = open("../config.json")
 current_release = json.load(f)
 if current_release["release"].strip('v') < latest_release:
