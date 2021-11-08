5fd180e7e6361731a9b10a910e3a98296ec24f99
unknown <vietng322611@gmail.com>
2021-11-08 13:12:30 +0700

    update file

---
+++ update.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import patoolib
+import os
+import time
+import urllib
+
+url = "https://api.github.com/repos/vietng322611/simple-python-discord-bot/releases/latest"
+response = requests.get(url)
+latest_release = response.json()["html_url"].strip("https://github.com/vietng322611/simple-python-discord-bot/releases/tag/v")
+f = open("../config.json")
+current_release = json.load(f)
+if current_release["release"].strip('v') < latest_release:
+   current_release = latest_release
+   print(f'Downloading release: {current_release}')
+   assets_url = response.json()["assets_url"]
+   package_url = requests.get(assets_url).json()["browser_download_url"]
+   urllib.request.urlretrieve(package_url, './')
+   patoolib.extract_archive("main.py", outdir='./')
+print('Done')
+time.sleep(2)
+exit()
