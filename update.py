842926a9284785040bda60a334ce299738680132
unknown <vietng322611@gmail.com>
2021-11-09 18:25:50 +0700

    still fixing

--- update.py
+++ update.py
@@ -6,15 +6,12 @@ import urllib
 
 url = "https://api.github.com/repos/vietng322611/music-bot-python/releases/latest"
 response = requests.get(url)
-<<<<<<< HEAD
 if response.status_code != 200:
-   print("Can't fetch latest version, abort")
+   print("Can't fetch latest release, abort")
    time.sleep(2)
    exit()
-=======
->>>>>>> 703b213ece930a1a2374452dce537b5d458d9cd4
 latest_release = response.json()["html_url"].strip("https://github.com/vietng322611/music-bot-python/releases/tag/v")
-f = open("../config.json")
+f = open("./config.json")
 current_release = json.load(f)
 if current_release["release"].strip('v') < latest_release:
    current_release = latest_release
@@ -24,10 +21,8 @@ if current_release["release"].strip('v') < latest_release:
    urllib.request.urlretrieve(package_url, './')
    patoolib.extract_archive("main.py", outdir='./')
    patoolib.extract_archive("config.json", outdir='./')
-print('Done')
-<<<<<<< HEAD
-time.sleep(2)
-=======
+   print('Done')
+else:
+   print('This is the latest release')
 time.sleep(2)
 exit()
->>>>>>> 703b213ece930a1a2374452dce537b5d458d9cd4
