c2c759f031935fa17f09189a1d6dce5c61719742
vietng <vietng322611@gmail.com>
2021-11-10 14:06:14 +0700

    Done

--- logger.py
+++ logger.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 import sys
 
-class logger():
+class logger:
+    config = {}
     def __init__(self, f):
-        self.config = f
+        self.file = f
         self.stdout = sys.stdout
-        self.log = open(self.config["Log_Path"] + 'log-' + str(datetime.now().date()) + '.txt', 'a')
+        self.log = open(self.file["Log_Path"] + 'log-' + str(datetime.now().date()) + '.txt', 'a')
     def flush(self):
         pass
     def write(self, text):      
--- update.py
+++ update.py
@@ -4,26 +4,23 @@ import time
 import urllib
 import os
 
-class update():
-   def __init__(self, f):
-      self.config = f  
-      self.release = self.config["Release"].strip("v")
-      self.url = "https://api.github.com/repos/vietng322611/music-bot-python/releases/latest"
-   def update(self):
-      response = requests.get(self.url)
-      if response.status_code != 200:
-         print("Can't fetch latest release, abort")
+def update(config):
+   release = config["Release"].strip("v")
+   url = "https://api.github.com/repos/vietng322611/music-bot-python/releases/latest"
+   response = requests.get(url)
+   if response.status_code != 200:
+      print("Can't fetch latest release, abort")
+   else:
+      latest_release = response.json()["tag_name"].strip("v")
+      if float(release) < float(latest_release):
+         print(f'Downloading release: {latest_release}')
+         package_url = response.json()["assets"][0]["browser_download_url"]
+         urllib.request.urlretrieve(package_url, "./bot-project.rar")
+         with ZipFile("bot-project.rar", 'r') as zip:
+            zip.extractall("./")
+         os.remove("bot-project.rar")
+         print('Update is done')
       else:
-         latest_release = response.json()["tag_name"].strip("v")
-         if float(self.release) < float(latest_release):
-            print(f'Downloading release: {latest_release}')
-            package_url = response.json()["assets"][0]["browser_download_url"]
-            urllib.request.urlretrieve(package_url, "./bot-project.rar")
-            with ZipFile("bot-project.rar", 'r') as zip:
-               zip.extractall("./")
-            os.remove("bot-project.rar")
-            print('Update is done')
-         else:
-            print("This is the latest release")
-      time.sleep(2)
-      return
+         print("This is the latest release")
+   time.sleep(2)
+   return
