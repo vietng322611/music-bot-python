c5026837dd59895a626f5c39dc14ae4ae899dc37
vietng <vietng322611@gmail.com>
2021-11-10 13:01:53 +0700

    Splited logger class into library

---
+++ logger.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+import sys
+
+class logger():
+    def __init__(self, config):
+        self.config = config
+        self.stdout = sys.stdout
+        self.log = open(self.config["Log_Path"] + 'log-' + str(datetime.now().date()) + '.txt', 'a')
+    def flush(self):
+        pass
+    def write(self, text):      
+        text = text.rstrip()
+        self.stdout.write('%s %s\n' % (datetime.now().strftime('%H:%M:%S'), text))
+        self.log.write('%s %s\n' % (datetime.now().strftime('%H:%M:%S'), text))
+        return
