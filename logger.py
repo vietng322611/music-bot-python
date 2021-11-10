68c7bfb800383d4c8147a49d86d6553ea7f14175
vietng <vietng322611@gmail.com>
2021-11-10 15:15:32 +0700

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,18 +1,17 @@
 # This made by vietng322611, please respect, do not copy without permission, you can change the code inside to your own.
 
 from datetime import datetime
-import sys
+from sys import stdout
 
 class logger:
     config = {}
     def __init__(self, f):
         self.file = f
-        self.stdout = sys.stdout
         self.log = open(self.file["Log_Path"] + 'log-' + str(datetime.now().date()) + '.txt', 'a')
     def flush(self):
         pass
     def write(self, text):      
         text = text.rstrip()
-        self.stdout.write('%s %s\n' % (datetime.now().strftime('%H:%M:%S'), text))
+        stdout.write('%s %s\n' % (datetime.now().strftime('%H:%M:%S'), text))
         self.log.write('%s %s\n' % (datetime.now().strftime('%H:%M:%S'), text))
         return
