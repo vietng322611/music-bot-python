3f804ae62fdfcd382dcbce8f4315c34f146e38de
vietng <vietng322611@gmail.com>
2021-11-10 15:15:10 +0700

    Update update.py

--- update.py
+++ update.py
@@ -10,17 +10,19 @@ def update(config):
    release = config["Release"].strip("v")
    url = "https://api.github.com/repos/vietng322611/music-bot-python/releases/latest"
    response = requests.get(url)
+   latest_release = response.json()["tag_name"].strip("v")
    if response.status_code != 200:
       print("Can't fetch latest release, abort")
+   elif response.json()["name"].strip(' ' + latest_release) != "Update":
+      print("This is the latest release")
    else:
-      latest_release = response.json()["tag_name"].strip("v")
       if float(release) < float(latest_release):
          print(f'Downloading release: {latest_release}')
          package_url = response.json()["assets"][0]["browser_download_url"]
-         urllib.request.urlretrieve(package_url, "./bot-project.rar")
-         with ZipFile("bot-project.rar", 'r') as zip:
+         urllib.request.urlretrieve(package_url, "./bot-project.zip")
+         with ZipFile("bot-project.zip", 'r') as zip:
             zip.extractall("./")
-         os.remove("bot-project.rar")
+         os.remove("bot-project.zip")
          print('Update is done')
       else:
          print("This is the latest release")
