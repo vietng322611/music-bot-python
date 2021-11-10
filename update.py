8b06c4c65379a6e8a0cefb508631cd686af393a1
vietng <vietng322611@gmail.com>
2021-11-10 13:02:23 +0700

    Converted to class update

--- update.py
+++ update.py
@@ -1,28 +1,29 @@
 import requests
-import json
-import patoolib
+from zipfile import ZipFile
 import time
 import urllib
+import os
 
-url = "https://api.github.com/repos/vietng322611/music-bot-python/releases/latest"
-response = requests.get(url)
-if response.status_code != 200:
-   print("Can't fetch latest release, abort")
-   time.sleep(2)
-   exit()
-latest_release = response.json()["html_url"].strip("https://github.com/vietng322611/music-bot-python/releases/tag/v")
-f = open("./config.json")
-current_release = json.load(f)
-if current_release["release"].strip('v') < latest_release:
-   current_release = latest_release
-   print(f'Downloading release: {current_release}')
-   assets_url = response.json()["assets_url"]
-   package_url = requests.get(assets_url).json()["browser_download_url"]
-   urllib.request.urlretrieve(package_url, './')
-   patoolib.extract_archive("main.py", outdir='./')
-   patoolib.extract_archive("config.json", outdir='./')
-   print('Done')
-else:
-   print('This is the latest release')
-time.sleep(2)
-exit()
+class update():
+   def __init__(self, config):
+      self.config = config   
+      self.release = self.config["Release"].strip("v")
+      self.url = "https://api.github.com/repos/vietng322611/music-bot-python/releases/latest"
+   def update(self):
+      response = requests.get(self.url)
+      if response.status_code != 200:
+         print("Can't fetch latest release, abort")
+      else:
+         latest_release = response.json()["tag_name"].strip("v")
+         if float(self.release) < float(latest_release):
+            print(f'Downloading release: {latest_release}')
+            package_url = response.json()["assets"][0]["browser_download_url"]
+            urllib.request.urlretrieve(package_url, "./bot-project.rar")
+            with ZipFile("bot-project.rar", 'r') as zip:
+               zip.extractall("./")
+            os.remove("bot-project.rar")
+            print('Update is done')
+         else:
+            print("This is the latest release")
+      time.sleep(2)
+      return
