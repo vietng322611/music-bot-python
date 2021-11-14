ddc9ce7aaabc5b3cc786d23bbaebede8d6455583
vietng <vietng322611@gmail.com>
2021-11-14 18:03:33 +0700

    Update update.py

---
+++ update.py
@@ -0,0 +1,35 @@
+# This made by vietng322611, please respect, do not copy without permission, you can change the code inside to your own.
+
+import requests
+from zipfile import ZipFile
+import time
+import urllib
+import os
+import json
+
+def update(config):
+   release = config["Release"].strip("v")
+   url = config["Update_Url"]
+   response = requests.get(url)
+   latest_release = response.json()["tag_name"].strip("v")
+   if response.status_code != 200:
+      print("Can't fetch latest release, abort")
+   if float(release) < float(latest_release):
+      config["Release"] = latest_release
+      if response.json()["name"].strip(' ' + latest_release) != "Update":
+         print(f"New release avalible, you can download it here: {url.replace('api.', '')}")
+      else:
+         print(f'New update avalible: {latest_release}')
+         print(f'Downloading update: {latest_release.strip("v")}')
+         package_url = response.json()["assets"][0]["browser_download_url"]
+         urllib.request.urlretrieve(package_url, "./bot-project.zip")
+         with ZipFile("bot-project.zip", 'r') as zip:
+            zip.extractall("./")
+         os.remove("bot-project.zip")
+         with open('config.json', 'w') as fp:
+            json.dump(config, fp)
+         print('Update is done')
+   else:
+      print('Everything is up to date')
+   time.sleep(2)
+   return
