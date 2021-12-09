680a0763e8b04d1f8c0bb695987fd33ff539d99e
vietng322611 <vietng322611@gmail.com>
2021-12-09 10:46:27 +0700

    Added release update

--- update.py
+++ update.py
@@ -7,19 +7,36 @@ import urllib
 import os
 import json
 
+def update_release(latest_release, config, response):
+   config["Release"] = latest_release
+   package_url = response.json()["assets"][0]["browser_download_url"]
+   urllib.request.urlretrieve(package_url, "./bot-project.zip")
+   with ZipFile("bot-project.zip", 'r') as zip:
+      zip.extractall("./")
+      os.remove("bot-project.zip")
+      with open('config.json', 'w') as fp:
+         json.dump(config, fp)
+      print('Update is done')
+   time.sleep(2)
+   return
+
 def update(config):
-   release = config["Release"].strip("v").strip(".")
+   update = config["Update"].strip("v").strip(".")
    url = config["Update_Url"]
    response = requests.get(url)
-   latest_release = response.json()["tag_name"].strip("v").strip(".")
+   latest_update = response.json()["tag_name"].strip("v").strip(".")
    if response.status_code != 200:
       print("Can't fetch latest release, abort")
-   if release < latest_release:
-      if response.json()["name"].strip(' ' + latest_release) != "Update":
-         print(f"New release avalible, you can download it here: {url.replace('api.', '')}")
+   if update < latest_update:
+      if response.json()["name"].strip(' ' + latest_update) != "Update":
+         config["Release"] = latest_update
+         print(f"New release avalible, downloading release {latest_update.strip('v')}")
+         update_release(latest_update, config, response)
+         return
       else:
-         print(f'New update avalible: {latest_release}')
-         print(f'Downloading update: {latest_release.strip("v")}')
+         config["Update"] = latest_update
+         print(f'New update avalible: {latest_update}')
+         print(f'Downloading update: {latest_update.strip("v")}')
          package_url = response.json()["assets"][0]["browser_download_url"]
          urllib.request.urlretrieve(package_url, "./bot-project.zip")
          with ZipFile("bot-project.zip", 'r') as zip:
