ec8a98d69b40bea285da0b8bd51316bb54f45501
vietng322611 <vietng322611@gmail.com>
2022-01-09 10:19:11 +0700

    Reinitialize

---
+++ cogs/UrlHandler.py
@@ -0,0 +1,45 @@
+from youtube_dl import YoutubeDL as youtubedl
+from bs4 import BeautifulSoup as bs
+
+import requests
+import re
+
+class url_exec():
+    def get_video_info(url):
+        r = requests.get(url)
+        s = bs(r.text, "html.parser")
+        title = s.find('title').get_text().replace(' - YouTube', '')
+        return title
+
+    def ytdl(url):
+        ydl_opts = {
+            'format': 'bestaudio',
+            'noplaylist': 'True',
+            'agelimit': '20',
+            'default_search': 'auto',
+            'audioformat': 'aac'
+        }
+        with youtubedl(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+            if 'entries' in info:
+                url = info['entries'][-1]['webpage_url']
+                url2 = info['entries'][0]['formats'][0]['url']
+                title = info['entries'][0]['title']
+                thumbnail = info['entries'][-1]['thumbnail']
+                duration = info['entries'][0]['duration']
+            else:
+                url2 = info['url']
+                title = info['title'] 
+                thumbnail = info['thumbnail']
+                duration = info['duration']
+        m1, s1 = divmod(int(duration), 60)
+        if len(str(s1)) == 1:
+            s1 = '0' + str(s1)
+        duration = '%s:%s' % (m1, s1)
+        return url, url2, title, thumbnail, duration
+
+    def request(input):
+        r = requests.get("https://www.youtube.com/results?search_query=" + input)
+        s = bs(r.text, "html.parser")
+        res = re.findall(r"watch\?v=(\S{11})", s.decode())
+        return res
---
+++ logger.py
@@ -0,0 +1,20 @@
+# This made by vietng322611, please respect, do not copy without permission, you can change the code inside to your own.
+
+from datetime import datetime
+from sys import stdout
+
+class logger:
+    def __init__(self, f):
+        self.file = f
+        self.log = open(self.file["Log_Path"] + 'log-' + str(datetime.now().date()) + '@' + str(datetime.now().strftime('%H.%M.%S')) + '.txt', 'w')
+        self.log.write('============================================\n')
+
+    def flush(self):
+        pass
+
+    def write(self, text):      
+        text = text.rstrip()
+        if text != '':
+           stdout.write('%s\n' % (text))
+           self.log.write('%s\n' % (text))
+        return
