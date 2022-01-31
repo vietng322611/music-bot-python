bfac21224913cb53a7dbc79ba620302df61d2539
vietng322611 <vietng322611@gmail.com>
2022-01-31 09:07:35 +0700

    Release update removed

--- update.py
+++ update.py
@@ -2,40 +2,18 @@
 
 import requests
 from zipfile import ZipFile
-import time
 import urllib
 import os
 import json
 import sys
 
-def update_release(latest_release, config, response):
-   config["Release"] = latest_release
-   package_url = response.json()["assets"][0]["browser_download_url"]
-   urllib.request.urlretrieve(package_url, "./bot-project.zip")
-   with ZipFile("bot-project.zip", 'r') as zip:
-      while zip.extractall("./"):
-         pass
-      os.remove("bot-project.zip")
-   with open('config.json', 'w') as fp:
-      json.dump(config, fp)
-   print('Update is done, restarting bot')
-   time.sleep(2)
-   
-
 def update(config):
    update = config["Update"].strip("v").strip(".")
-   release = config["Release"].strip("v").strip(".")
    url = config["Update_Url"]
    response = requests.get(url)
    latest_update = response.json()["tag_name"].strip("v").strip(".")
    if response.status_code != 200:
       print("Can't fetch latest release, abort")
-   elif response.json()["name"].strip(' ' + 'v' + latest_update) != "Update":
-      print(response.json()["name"])
-      if release < latest_update:
-         print(f"New release avalible, downloading release {latest_update.strip('v')}")
-         update_release(latest_update, config, response)
-      return
    elif update < latest_update:
       print(f'New update avalible: {latest_update}')
       print(f'Downloading update: {latest_update.strip("v")}')
@@ -48,6 +26,7 @@ def update(config):
       with open('config.json', 'w') as fp:
          json.dump(config, fp)
       print('Update is done, restarting bot')
+      os.execl(sys.executable, sys.executable, *sys.argv)
    else:
       print('Everything is up to date')
-   os.execl(sys.executable, sys.executable, *sys.argv)
+   return
