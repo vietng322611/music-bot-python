a59134de6ff73afe992344907c2dcfdd126166ef
vietng322611 <vietng322611@gmail.com>
2022-02-09 21:10:05 +0700

    Update voice state func + experiment welcome event

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ from discord.ext import commands
 from discord.utils import get
 from discord.embeds import Embed
 from discord.colour import Color
+from discord import Intents
 from logger import logger
 from update import update
 from dotenv import load_dotenv
 from cogs.music import music
+from keep_alive import keep_alive
 
+keep_alive()
 config = json.load(open('./config.json'))
 if os.path.exists('logs'):
     sys.stdout = logger(config)
@@ -24,16 +27,18 @@ else:
 if config["Check_Update_On_Start"] == "True":
     update(config)
 
-load_dotenv()
+load_dotenv('token.env')
 TOKEN = os.getenv("BOT_TOKEN")
 
-bot = commands.Bot(description="A simple bot made by vietng322611", command_prefix=config["Prefix"])
+intents = Intents.default()
+intents.members = True
+bot = commands.Bot(description="A simple bot made by vietng322611", command_prefix=config["Prefix"], intents=intents)
 bot.add_cog(music(bot))
 queue = []
 queue_info = []
 banned_words_spam = {}
 creator = config["Creator_Id"]
-banned_words = ['lỏd'] # if you don't need you can delete it
+banned_words = [] # If someone annoy you by spamming words, ban that word
 
 async def voice_check(voice):
     if voice != None:
@@ -81,14 +86,25 @@ async def on_voice_state_update(member, before, after):
     if member.name == bot.user.name:
         return
     elif bot.voice_clients != []:
-        voice = bot.voice_clients[0]
-        if after.channel != voice:
-            await voice_check(voice)
+        for voice in bot.voice_clients:
+            if after.channel == voice:
+                return
+        await voice_check(voice)
+
+@bot.event
+async def on_member_join(member):
+    server = bot.get_server(member.server)
+    await get(server.text_channels, name='welcome').send(f"{member.name} has joined")
+
+@bot.event
+async def on_member_remove(member):
+    server = bot.get_server(member.server)
+    await get(server.text_channels, name='welcome').send(f"{member.name} has leaved")
 
 @bot.command(name='banned-words', help='Show a list of banned words')
 async def show_banned_words(ctx):
     embed = Embed(color = Color.from_rgb(255, 0, 0))
     embed.add_field(name="Banned words", value=banned_words, inline=False)
-    await ctx.message.send(embed=embed)
+    await ctx.message.channel.send(embed=embed)
 
 bot.run(TOKEN)
