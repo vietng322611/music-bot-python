ff94b006a90692d32a073b1a0982ddb01fdf0518
vietng322611 <vietng3225611@gmail.com>
2022-02-13 07:55:48 +0700

    Added time that it executes the command

--- logger.py
+++ logger.py
@@ -1,5 +1,3 @@
-# This made by vietng322611, please respect, do not copy without permission, you can change the code inside to your own.
-
 from datetime import datetime
 from sys import stdout
 
@@ -7,17 +5,16 @@ class logger:
     def __init__(self, f):
         self.file = f
         self.log = open(self.file["Log_Path"] + 'log-' + str(datetime.now().date()) + '@' + str(datetime.now().strftime('%H.%M.%S')) + '.txt', 'w')
-        self.log.write('============================================\n')
 
     def flush(self):
         pass
 
-    def write(self, text):      
+    def write(self, text):
         text = text.rstrip()
         if text != '':
             try:
-                stdout.write('%s\n' % (text))
-                self.log.write('%s\n' % (text))
+                stdout.write(f"[{datetime.now().strftime('%H.%M.%S')}]" + ' %s\n' % (text))
+                self.log.write(f"[{datetime.now().strftime('%H.%M.%S')}]" + ' %s\n' % (text))
             except UnicodeEncodeError:
                 return
         return
