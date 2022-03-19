67d2d5dc81de0bdf23e69bc58e9836d23f7ee0c2
vietng322611 <vietng3225611@gmail.com>
2022-03-19 10:09:49 +0700

    Update search function

--- cogs/UrlHandler.py
+++ cogs/UrlHandler.py
@@ -4,46 +4,44 @@ from bs4 import BeautifulSoup as bs
 import requests
 import re
 
-class url_exec():
-    def get_video_info(url):
-        r = requests.get(url)
-        s = bs(r.text, "html.parser")
-        title = s.find('title').get_text().replace(' - YouTube', '')
-        return title
+def get_video_info(url):
+    r = requests.get(url)
+    s = bs(r.text, "html.parser")
+    title = s.find('title').get_text().replace(' - YouTube', '')
+    return title
 
-    def ytdl(url):
-        ydl_opts = {
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'aac',
-                'preferredquality': '192',
-            }],
-            'format': 'bestaudio',
-            'agelimit': '20',
-            'noplaylist': 'True',
-            'default_search': 'auto'
-        }
-        with youtubedl(ydl_opts) as ydl:
-            info = ydl.extract_info(url, download=False)
-            if 'entries' in info:
-                url = info['entries'][-1]['webpage_url']
-                url2 = info['entries'][0]['formats'][0]['url']
-                title = info['entries'][0]['title']
-                thumbnail = info['entries'][-1]['thumbnail']
-                duration = info['entries'][0]['duration']
-            else:
-                url2 = info['url']
-                title = info['title'] 
-                thumbnail = info['thumbnail']
-                duration = info['duration']
-        m1, s1 = divmod(int(duration), 60)
-        if len(str(s1)) == 1:
-            s1 = '0' + str(s1)
-        duration = '%s:%s' % (m1, s1)
-        return url, url2, title, thumbnail, duration
-
-    def request(input):
-        r = requests.get("https://www.youtube.com/results?search_query=" + input)
-        s = bs(r.text, "html.parser")
+def search(opt, input):
+    r = requests.get("https://www.youtube.com/results?search_query=" + input)
+    s = bs(r.text, "html.parser")
+    if opt == 'one':
+        res = re.search(r"watch\?v=(\S{11})", s.decode())
+        return res
+    elif opt == 'all':
         res = re.findall(r"watch\?v=(\S{11})", s.decode())
         return res
+
+def ytdl(url):
+    if not url.startswith(('https://www.youtube.com/watch?v=', 'https://youtu.be/')):
+        url = 'https://www.youtube.com/watch?v=' + search('one', url)[0]
+    ydl_opts = {
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'aac',
+            'preferredquality': '192',
+        }],
+        'format': 'bestaudio',
+        'agelimit': '20',
+        'noplaylist': 'True',
+        'default_search': 'auto'
+    }
+    with youtubedl(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        url2 = info['url']
+        title = info['title'] 
+        thumbnail = info['thumbnail']
+        duration = info['duration']
+    m1, s1 = divmod(int(duration), 60)
+    if len(str(s1)) == 1:
+        s1 = '0' + str(s1)
+    duration = '%s:%s' % (m1, s1)
+    return url, url2, title, thumbnail, duration
