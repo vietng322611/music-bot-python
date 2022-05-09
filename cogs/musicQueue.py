6c317f823a1a72a1d272879914d9123d7beb0918
vietng322611 <vietng3225611@gmail.com>
2022-05-09 07:40:57 +0700

    Added return for getPlayer func

--- cogs/musicQueue.py
+++ cogs/musicQueue.py
@@ -1,10 +1,10 @@
 from youtube_dl import YoutubeDL as youtubedl
 
 class musicQueue():
-    def __init__(self, url, title, thumbnail):
+    def __init__(self, url, title, thumbnail_url):
         self.url = url
         self.title = title
-        self.thumbnail_url = thumbnail
+        self.thumbnail_url = thumbnail_url
 
     def getPlayer(self):
         ydl_opts = {
@@ -22,3 +22,4 @@ class musicQueue():
             info = ydl.extract_info(self.url, download=False)
             url2 = info['url']
             duration = info['duration']
+        return ur2, duration
