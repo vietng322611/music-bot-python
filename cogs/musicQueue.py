8bb89e421e61981ebcb0fb6b195396d881f410cd
vietng322611 <vietng3225611@gmail.com>
2022-05-09 07:07:41 +0700

    initial commit

---
+++ cogs/musicQueue.py
@@ -0,0 +1,24 @@
+from youtube_dl import YoutubeDL as youtubedl
+
+class musicQueue():
+    def __init__(self, url, title, thumbnail):
+        self.url = url
+        self.title = title
+        self.thumbnail = thumbnail
+
+    def getPlayer(self):
+        ydl_opts = {
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'aac',
+            'preferredquality': '192',
+        }],
+        'format': 'bestaudio',
+        'agelimit': '20',
+        'noplaylist': 'True',
+        'default_search': 'auto'
+        }
+        with youtubedl(ydl_opts) as ydl:
+            info = ydl.extract_info(self.url, download=False)
+            url2 = info['url']
+            duration = info['duration']
