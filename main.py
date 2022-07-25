48cd8f87983a00eb70d090817c15ccbdd115b48a
vietng322611 <vietng3225611@gmail.com>
2022-07-25 13:48:01 +0700

    removed function with intents

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # This made by vietng322611, please be respect, don't copy it without permission, you can change the code inside into your own.
-# If something goes wrong, DM me your log or try restarting the bot.
+# If something goes wrong, DM me (murasaki#1843) your log or try restarting the bot.
+
+''' TODO:
+    + Make a spammer detector
+    + Clean app after exit
+    + Rebuild logger.py
+ '''
+# ! Fix certains bugs cause app to crash
 
 import modwall; modwall.check() # Library checker
 
@@ -8,11 +15,8 @@ import os
 import json
 import discord
 
-from collections import defaultdict
 from discord.ext import commands
 from discord.utils import get
-from discord.embeds import Embed
-from discord.colour import Color
 from logger import logger
 from update import update
 from dotenv import load_dotenv
@@ -20,8 +24,8 @@ from cogs.music import music
 from cogs.extent import extent
 from gtts import gTTS
 from time import sleep
-from datetime import datetime, timedelta
 
+# Check config file directory
 config = json.load(open('./config.json'))
 if not os.path.exists('logs'):
     os.mkdir('logs')
@@ -33,17 +37,12 @@ if config["Check_Update_On_Start"] == "True":
 load_dotenv('token.env')
 TOKEN = os.getenv("BOT_TOKEN")
 
-intents = discord.Intents.default()
-intents.presences = True
-intents.members = True
-bot = commands.Bot(description="I'm a happy bot", command_prefix=config["Prefix"], intents=intents)
+bot = commands.Bot(description="", command_prefix=config["Prefix"]) 
 bot.add_cog(music(bot, config))
 bot.add_cog(extent(bot, config))
-queue = []
-queue_info = []
-banned_words_spam = defaultdict(int)
+queue = [] # Music queue
+queue_info = [] # Music info
 creator = config["Creator_Id"]
-banned_words = config["Banned_Words"]
 
 async def voice_check(voice, channel): # Check if only bot in voice channel
     member_count = len(channel.members)
@@ -69,19 +68,6 @@ async def on_message(message):
     inp = message.content
     if message.author == bot.user:
         return
-    if message.author.id != creator:
-        for i in banned_words:
-            if i + " " in inp.lower() or inp.lower() == i:
-                await message.delete()
-                if banned_words_spam[message.author.id] > 4:
-                    await message.author.timeout(datetime.now() + timedelta(minutes = 5))
-                    await message.channel.send(f"<@{message.author.id}> timed out user for 5 minutes for spamming banned word.", delete_after=5)
-                    banned_words_spam[message.author.id] = 0
-                    return
-
-                else:
-                    banned_words_spam[message.author.id] += 1
-                    return await message.channel.send(f"<@{message.author.id}> used banned word. Subsequent violations may be banned by creator.", delete_after=5)
     await bot.process_commands(message)
 
 @bot.event
@@ -104,22 +90,5 @@ async def on_voice_state_update(member, before, after): # Get voice status
               voice.play(discord.FFmpegPCMAudio('gg.mp3'))
               voice.source = discord.PCMVolumeTransformer(voice.source, volume=2.0)
               return
-@bot.event
-async def on_member_join(member): #testing
-    server = bot.get_server(member.server)
-    print(member)
-    await get(server.text_channels, name='welcome').send(f"{member.name} has joined")
-
-@bot.event
-async def on_member_remove(member): #testing
-    server = bot.get_server(member.server)
-    print(member)
-    await get(server.text_channels, name='welcome').send(f"{member.name} has leaved")
-
-@bot.command(name='banned-words', help='Show a list of banned words')
-async def show_banned_words(ctx):
-  embed = Embed(color = Color.from_rgb(255, 0, 0))
-  embed.add_field(name="Banned words", value=banned_words, inline=False)
-  await ctx.message.channel.send(embed=embed)
 
 bot.run(TOKEN)
